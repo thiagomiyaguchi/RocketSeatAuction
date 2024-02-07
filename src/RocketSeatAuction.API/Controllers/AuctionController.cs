2cbff165d2a66c7329ad8df57f040eab940368a3
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-02-06 22:04:08 -0300

    Add project files.

---
+++ src/RocketSeatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RocketSeatAuction.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction()
+    {
+        return Ok("Okaay");
+    }
+
+
+
+}
